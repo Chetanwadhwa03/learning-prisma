531a0e27630e3ab3b1ee92debdf254f37ef61aaa
Chetanwadhwa03 <chetanwadhwa03@gmail.com>
2026-07-08 10:21:26 +0530

    Created a entry in DB using prisma

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,22 +1,18 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client.js";
-
-
 const prisma = new PrismaClient({
     adapter: new PrismaPg({ connectionString: process.env["DATABASE_URL"] })
 });
-
-const createUser = async (username:string, password:string, firstname:string, lastname:string)=>{
+const createUser = async (username, password, firstname, lastname) => {
     const res = await prisma.users.create({
-        data:{
+        data: {
             username,
             password,
             firstname,
             lastname
         }
-    })
-
+    });
     console.log(res);
-}
-
-createUser("chetan03","123","Chetan","wadhwa");
+};
+createUser("chetan03", "123", "Chetan", "wadhwa");
+//# sourceMappingURL=index.js.map
